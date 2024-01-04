5d80d467dd5f310e0f54f6c5a84e166b5688aa24
Alan Nahuel <96840359+alanahuel@users.noreply.github.com>
2024-01-04 01:53:01 +0100

    part 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://my-astroblog.netlify.app/'
+  site: 'https://my-astroblog.netlify.app/',
+  integrations: [preact()]
 });
